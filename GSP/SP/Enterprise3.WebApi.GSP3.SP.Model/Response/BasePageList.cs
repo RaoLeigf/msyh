90d19e0c4dcb89c3ba0950234107f3f77aecfda4
rl <1020209543@qq.com>
2020-03-20 11:39:10 +0800

    修复审批人员超过30不分页bug_2

---
+++ GSP/SP/Enterprise3.WebApi.GSP3.SP.Model/Response/BasePageList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enterprise3.WebApi.GSP3.SP.Model.Response
+{
+    public class BasePageList<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<T> Data { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int TotalCount { get; set; }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PageSize { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public int PageIndex { get; set; }
+    }
+}
